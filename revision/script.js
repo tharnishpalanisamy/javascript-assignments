97a02adee2a3c026e95b1d01c23e6ec891fbfa80
Tharnish Palanisamy <tharnishpalanisamy3@gmail.com>
2026-07-03 17:22:12 +0530

    class revision

---
+++ revision/script.js
@@ -0,0 +1,17 @@
+// class Student {
+//     constructor(name , age){
+//         this.name = name 
+//         this.age = age 
+//     }
+
+//     display(){
+//         console.log(`${this.name} ${this.age}`);
+        
+//     }
+// }
+
+
+// let Tharnish = new Student('Tharnish' , 20) 
+// let Naveen = new Student('Naveen' , 20) 
+// Tharnish.display() 
+// Naveen.display()
