9354116f9db256d7a055e7606c5e07ddf93ab6e8
Tharnish Palanisamy <tharnishpalanisamy3@gmail.com>
2026-07-04 12:42:26 +0530

    attributes

--- revision/dom.js
+++ revision/dom.js
@@ -1,23 +1,25 @@
 let elements = document.getElementById('title') 
-console.log(elements.innerHTML);
+// console.log(elements.innerHTML);
 
-let h1 = document.querySelector('h1') 
-console.log(h1.innerText);
+// let h1 = document.querySelector('h1') 
+// console.log(h1.innerText);
 
-let p = document.querySelectorAll('p') 
+// let p = document.querySelectorAll('p') 
 
-p.forEach(i =>console.log(i)
-)
+// p.forEach(i =>console.log(i)
+// )
 
-let text = document.getElementsByClassName('text') 
-console.log(text); 
+// let text = document.getElementsByClassName('text') 
+// console.log(text); 
 
-let btn = document.getElementsByTagName('button')
-console.log(btn);
+// let btn = document.getElementsByTagName('button')
+// console.log(btn);
 
 
-console.log(box.innerHTML);
+// console.log(box.innerHTML);
 
-console.log(box.innerText);
+// console.log(box.innerText);
 
-console.log(box.textContent);
+// console.log(box.textContent);
+
+console.log(elements.getAttribute('id'));  
