840462111f9b5f986c61654373ea4ca19ae15d6b
Tharnish Palanisamy <tharnishpalanisamy3@gmail.com>
2026-07-04 11:56:50 +0530

    dom selectors

---
+++ revision/dom.js
@@ -0,0 +1,23 @@
+let elements = document.getElementById('title') 
+console.log(elements.innerHTML);
+
+let h1 = document.querySelector('h1') 
+console.log(h1.innerText);
+
+let p = document.querySelectorAll('p') 
+
+p.forEach(i =>console.log(i)
+)
+
+let text = document.getElementsByClassName('text') 
+console.log(text); 
+
+let btn = document.getElementsByTagName('button')
+console.log(btn);
+
+
+console.log(box.innerHTML);
+
+console.log(box.innerText);
+
+console.log(box.textContent);
